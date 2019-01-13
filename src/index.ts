8ca653bb0201d6b5f273e279502bc39200b4eda1
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-01-12 21:35:16 -0500

    Added Login mutations and resolvers to save he cookie for the current logged user

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,51 @@ import Express from "express";
 import { buildSchema, formatArgumentValidationError } from "type-graphql";
 import { createConnection } from "typeorm";
 import { RegisterResolver } from "./modules/user/Register";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { redis } from "./redis";
+import cors from "cors";
+import { LoginResolver } from "./modules/user/Login";
+import { MeResolver } from "./modules/user/Me";
 
 const main = async () => {
   await createConnection();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver]
+    resolvers: [MeResolver, RegisterResolver, LoginResolver]
   });
 
   const apolloServer = new ApolloServer({
     schema,
-    formatError: formatArgumentValidationError
+    formatError: formatArgumentValidationError,
+    context: ({ req }: any) => ({ req })
   });
 
   const app = Express();
+  const RedisStore = connectRedis(session);
+  app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000"
+    })
+  );
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis as any
+      }),
+      name: "qid",
+      secret: "duhfsiudfgsdf",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365
+      }
+    })
+  );
 
   apolloServer.applyMiddleware({ app });
 
